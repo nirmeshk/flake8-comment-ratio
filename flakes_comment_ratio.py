b3bd786cc3eab170e676296bd2c174af85d41a47
Nirmesh <nirmeshk@users.noreply.github.com>
2015-10-21 17:41:15 -0400

    Update flakes_comment_ratio.py

--- flakes_comment_ratio.py
+++ flakes_comment_ratio.py
@@ -13,14 +13,10 @@ class CommentToCodeRatio(object):
         self.filename = (filename == 'stdin' and stdin) or filename
 
     def run(self):
-        if self.filename == stdin:
-            noqa = get_noqa_lines(self.filename)
-        else:
-            with open(self.filename, 'r') as file_to_check:
-                code, comment = get_comment_count(file_to_check.readlines())
-
-        if( comment/code < 0.05 ):
-            yield (1, 1, COMMENT_ERROR_MESSAGE, COMMENT_ERROR_CODE)
+        with open(self.filename, 'r') as file_to_check:
+            code, comment = get_comment_count(file_to_check.readlines())
+            if( code > 0 and comment/code < 0.05 ):
+                yield (1, 1, COMMENT_ERROR_MESSAGE, COMMENT_ERROR_CODE)
 
 def get_comment_count(fname):
     """ Run on just one file.
@@ -51,15 +47,4 @@ def get_comment_count(fname):
     return code, docstring+comment
 
 if __name__ == '__main__':
-    do_file(sys.argv[1])
-
-
-
-
-class CommentToCodeRatio(object):
-    """ This is a docstring
-    asdasdasdas"""
-    print("Asdadas") #This is non-docstring comment
-    print("asdasdadasdadasdsada")
-    # sdfsdfsd
-    # sjdasdhjasdkj
+    get_comment_count(sys.argv[1])
