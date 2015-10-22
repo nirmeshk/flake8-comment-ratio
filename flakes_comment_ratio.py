e56f7b73c1e6573024ff2492bee0663e021338fe
Nirmesh <nirmeshk@users.noreply.github.com>
2015-10-21 20:44:49 -0400

    Update flakes_comment_ratio.py

--- flakes_comment_ratio.py
+++ flakes_comment_ratio.py
@@ -14,35 +14,34 @@ class CommentToCodeRatio(object):
         self.filename = (filename == 'stdin' and stdin) or filename
 
     def run(self):
-        with open(self.filename, 'r') as file_to_check:
-            code, comment = get_comment_count(file_to_check)
-            if( code > 0 and comment/code < 0.5 ):
-                yield (1, 1, COMMENT_ERROR_MESSAGE, COMMENT_ERROR_CODE)
+        code, comment = get_comment_count(self.filename)
+        if( code > 0 and comment/code < 0.5 ):
+            yield (1, 1, COMMENT_ERROR_MESSAGE, COMMENT_ERROR_CODE)
 
-def get_comment_count(file_content):
+def get_comment_count(f):
     """ Run on just one file.
     """
     prev_toktype = token.INDENT
     comment = 0
     docstring = 0
     code = 0
-
-    tokgen = tokenize.generate_tokens(file_content)
-    for toktype, ttext, (slineno, scol), (elineno, ecol), ltext in tokgen:
-        if toktype == token.STRING and prev_toktype == token.INDENT:
-            # Docstring
-            docstring += 1
-            print("Docstring")
-        elif toktype == tokenize.COMMENT:
-            # Comment
-            comment += 1
-            print("COMMENT")
-        else:
-            code += 1
-
-        prev_toktype = toktype
-        last_col = ecol
-        last_lineno = elineno
+    with open(f, 'r') as file_to_check:
+        tokgen = tokenize.generate_tokens(file_to_check)
+        for toktype, ttext, (slineno, scol), (elineno, ecol), ltext in tokgen:
+            if toktype == token.STRING and prev_toktype == token.INDENT:
+                # Docstring
+                docstring += 1
+                print("Docstring")
+            elif toktype == tokenize.COMMENT:
+                # Comment
+                comment += 1
+                print("COMMENT")
+            else:
+                code += 1
+    
+            prev_toktype = toktype
+            last_col = ecol
+            last_lineno = elineno
     print("comments: ", docstring+comment )
     return code, docstring+comment
 
